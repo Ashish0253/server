7cc2d27165df1d3e5499fdf6b05b62fee0be2540
Ashish0253 <ashishrana0253@gmail.com>
2023-01-07 19:58:25 +0530

    installed cross-fetch for fetch issue in netlify

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const fetch = require("node-fetch");
+const fetch = require("cross-fetch");
+// import fetch from "node-fetch";
 
 const serverless = require("serverless-http");
 
